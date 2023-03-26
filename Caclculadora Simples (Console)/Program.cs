c90dd332e8db9ea9c7944a6a2bc512b735f0234b
Joaov <Joaov@JOAO>
2023-03-26 18:14:29 -0300

    Adicionar arquivos de projeto.

---
+++ Caclculadora Simples (Console)/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mensagem de boas-vindas
+            Console.WriteLine("Bem-vindo à calculadora!");
+
+            // Pedindo o primeiro número
+            Console.Write("Digite o primeiro número: ");
+            double numero1 = double.Parse(Console.ReadLine());
+
+            // Pedindo o segundo número
+            Console.Write("Digite o segundo número: ");
+            double numero2 = double.Parse(Console.ReadLine());
+
+            // Pedindo a operação desejada
+            Console.Write("Digite a operação desejada (+, -, *, /): ");
+            string operacao = Console.ReadLine();
+
+            // Verificando a operação desejada e calculando o resultado
+            double resultado = 0;
+            switch (operacao)
+            {
+                case "+":
+                    resultado = numero1 + numero2;
+                    break;
+                case "-":
+                    resultado = numero1 - numero2;
+                    break;
+                case "*":
+                    resultado = numero1 * numero2;
+                    break;
+                case "/":
+                    resultado = numero1 / numero2;
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida!");
+                    break;
+            }
+
+            // Exibindo o resultado
+            Console.WriteLine("O resultado da operação é: " + resultado);
+
+            // Esperando o usuário pressionar qualquer tecla para encerrar o programa
+            Console.WriteLine("Pressione qualquer tecla para encerrar o programa.");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+// AUTOR JOÃO VICTOR APARECIDO
+// DATA  20/03/2023
+//PROJETO PARA TREINAR
+//OBRIGADO POR BAIXAR O MEU PROJETO
